a3904e19262820eb26dc502587ba2391e75d8ba1
harmonilarrabee <32171203+harmonilarrabee@users.noreply.github.com>
2017-12-12 12:40:23 -0500

    User can select derivative type and number of terms

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+terms = []
+
 def main():
+	printHeader()
 	while True:
 		derivativeType = int(getDerivativeType())
 		while not derivativeType in range (0,5):
-			printErrorMessage()
+			derivativeType = int(errorMessage())
 		if derivativeType == 0:
 			printPowerRuleDerivative()
 		elif derivativeType == 1:
@@ -14,5 +17,63 @@ def main():
 		elif derivativeType == 4:
 			sayGoodbye()
 			break
+
+def getDerivativeType():
+	printInputQuestions()
+	return input("Type selection and press enter: ")
+
+def printHeader():
+	print ("")
+	print ("Hello! Welcome to the Derivative Calculator! ")
+	print ("")
+
+def printInputQuestions():
+	print("What type of function do you want to derive? ")
+	print ("To derive a function with the power rule, press 0. ")
+	print ("To derive a function with the product rule, press 1. ")
+	print ("To derive a function with the quotient rule, press 2. ")
+	print ("To derive a function with the chain rule, press 3. ")
+	print ("If you don't want to derive a function, press 4. ")
+	print ("")
+
+def errorMessage():
+	print ("")
+	return input("I'm sorry, that's not one of the options. Please type either 0, 1, 2, or 3: ")
+
+def printPowerRuleDerivative():
+	getFunction()
+	print ("")
+
+def printProductRuleDerivative():
+	getFunction()
+	print ("")
+
+def printQuotientRuleDerivative():
+	getFunction()
+	print ("")
+
+def printChainRuleDerivative():
+	getFunction()
+	print ("")
+
+def getFunction():
+	print ("")
+	numberOfTerms = int(input("PLease enter the number of terms in the function: "))
+	while not numberOfTerms in range (1,6):
+		print ("")
+		print ("I'm sorry, that's not a valid number of terms. ")
+		numberOfTerms = int(input("Please enter an integer between 1 and 5: "))
+		print ("")
+	for i in range (0, numberOfTerms):
+		getTerm()
+
+def getTerm():
+	pass
+
+def sayGoodbye():
+	print ("")
+	print ("Okay, Goodbye! ")
+	print ("")
+
 
 main()
